d346d7922e3d86c31cf5b52ce332788339b4eac3
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-11-26 22:34:26 -0500

    fix(input): fix definition for default
    Closes #1

--- src/questions/input.ts
+++ src/questions/input.ts
@@ -1,18 +1,18 @@
-import { BaseQuestion, Message, When } from './base';
+import { BaseQuestion, Message, When, Default } from './base';
 
 /* Input Question */
 export interface InputQuestion extends BaseQuestion {
   type: 'input';
   filter?: (answer: string) => any | Promise<any>;
   validate?: (answer: string) => boolean | Promise<boolean> | string | Promise<string>;
-  default?: string;
+  default?: Default<string>;
 }
 
 export function input (name: string, message: Message, options?: {
   when?: When;
   filter?: (answer: string) => any | Promise<any>;
   validate?: (answer: string) => boolean | Promise<boolean> | string | Promise<string>;
-  default?: string;
+  default?: Default<string>;
 }): InputQuestion {
   return Object.assign({}, { type: 'input' as 'input', name, message}, options);
 }
